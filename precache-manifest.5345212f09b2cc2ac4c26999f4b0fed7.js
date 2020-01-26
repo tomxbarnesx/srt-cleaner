897f55ee77727dc274bff0d2a6b582b0a2b7e3cc
Tom Barnes <barnes.thomas.c@gmail.com>
2020-01-26 11:54:37 -0500

    Updates

---
+++ precache-manifest.5345212f09b2cc2ac4c26999f4b0fed7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6ac3d5653312a4d267af5b4ab8160f0",
+    "url": "/srt-cleaner/index.html"
+  },
+  {
+    "revision": "bc9f10e4030b6aec9861",
+    "url": "/srt-cleaner/static/css/main.cc579e95.chunk.css"
+  },
+  {
+    "revision": "8d753c6d43690a4894a4",
+    "url": "/srt-cleaner/static/js/2.0dac2ea8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/srt-cleaner/static/js/2.0dac2ea8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bc9f10e4030b6aec9861",
+    "url": "/srt-cleaner/static/js/main.e6989d93.chunk.js"
+  },
+  {
+    "revision": "9407eff9849b6a24f51f",
+    "url": "/srt-cleaner/static/js/runtime-main.d4955313.js"
+  }
+]);
